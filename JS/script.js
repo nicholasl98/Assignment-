8bbe68ed899114c27caff045b57a4bec832f1c18
Nicholas Lai <nicholas_lai_1@hotmail.com>
2020-12-06 18:07:01 -0800

    JS and css updated files for assignment 2

---
+++ JS/script.js
@@ -0,0 +1,61 @@
+let mode_btn = document.getElementById('mode')
+mode_btn.addEventListener('click',changemode)
+
+function changemode(){
+    if (mode_btn.innerHTML == 'Dark'){
+        document.body.style.backgroundColor= '#656565'
+        mode_btn.style.backgroundColor = '#ECE8E8'
+        mode_btn.style.color = 'black'
+        document.querySelector('nav').style.backgroundColor='#656565'
+        document.querySelector('section').style.backgroundColor='#3A3A3A'
+        mode_btn.innerHTML = 'Light'
+    }
+    else{
+        mode_btn.innerHTML = 'Dark'
+        document.body.style.backgroundColor = '#ECE8E8'
+        mode_btn.style.color = 'white'
+        mode_btn.style.backgroundColor= '#656565'
+        document.querySelector('nav').style.backgroundColor='#C4C4C4'
+        document.querySelector('section').style.backgroundColor='#C4C4C4'
+    }
+}
+
+let new_btn = document.getElementById('newnote')
+new_btn.addEventListener('click',newnote)
+
+
+function newnote(){
+        var x = document.getElementById("boxbutton");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    
+}
+
+let cancel_btn = document.getElementById('Cancel')
+cancel_btn.addEventListener('click', removeelement)
+
+function removeelement(){
+    var x = document.getElementById("boxbutton");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+arr = [
+
+    {title: "note one", body: "note 1"},
+
+    {title: "Notes two", body: "hello sir"}, ];
+
+let save_btn = document.getElementById('save')
+save_btn.addEventListener('click',)
+
+function savenote(){
+
+}
